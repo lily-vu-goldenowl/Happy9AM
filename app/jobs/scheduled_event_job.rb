eae0b52cfbb399ad06aed5230455b32b54dc8ee1
lily-vu-goldenowl <lily.vu.goldenowl@gmail.com>
2025-03-18 21:43:31 +0700

    refactor: the event scheduler event job

--- app/jobs/scheduled_event_job.rb
+++ app/jobs/scheduled_event_job.rb
@@ -7,24 +7,17 @@ class ScheduledEventJob
 
     User.today_is_birthday.find_each do |user|
       event_date = user.next_event_date(event_type)
-      next if event_date.nil?
+
+      next if !event_date&.today?
 
       today = Time.now.in_time_zone(user.timezone).to_date
-      event_today = event_date == today
 
-      if event_today && !strategy.already_sent?(user, today)
+      if !strategy.already_sent?(user, today)
         # *IMPORTANT: Ensure this job is only enqueued once per user per day.
         next unless EventJobLock.lock(event_type, user.id)
 
-        current_time = Time.now.in_time_zone(user.timezone)
-        target_hour = hour || current_time.hour
-
-        if current_time.hour >= target_hour
-          DeliverEventMessageJob.perform_later(user.id, event_type, hour)
-        else
-          event_time = strategy.calculate_event_time(today, user.timezone)
-          DeliverEventMessageJob.set(wait_until: event_time).perform_later(user.id, event_type, hour)
-        end
+        event_time = strategy.calculate_event_time(today, user.timezone)
+        DeliverEventMessageJob.set(wait_until: event_time).perform_later(user.id, event_type, hour)
       end
     end
   end
--- app/services/event_scheduler_service.rb
+++ app/services/event_scheduler_service.rb
@@ -21,7 +21,7 @@ class EventSchedulerService
     def schedule_event(user, strategy)
       today = Time.now.in_time_zone(user.timezone).to_date
       event_date = user.next_event_date(strategy.event_type)
-      return if event_date.nil? || event_date != today || !EventJobLock.lock(strategy.event_type, user.id)
+      return if !event_date&.today? || !EventJobLock.lock(strategy.event_type, user.id)
 
       event_time = strategy.calculate_event_time(event_date, user.timezone)
       DeliverEventMessageJob.set(wait_until: event_time).perform_later(user.id, strategy.event_type, strategy.hour)
